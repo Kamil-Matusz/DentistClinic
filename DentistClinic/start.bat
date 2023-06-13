28f4428ddac37d708a001feac298b2600399fca7
Awahir <matuszkamil076@gmail.com>
2023-06-13 16:24:23 +0200

    Start script refactor

--- DentistClinic/start.bat
+++ DentistClinic/start.bat
@@ -1,11 +1,8 @@
 %systemDrive%\xampp\mysql\bin\mysql -uroot -e "CREATE DATABASE IF NOT EXISTS dentistclinic;"
-php -r "copy('.env.example', '.env');"
 call composer install
 call php artisan key:generate
 call php artisan storage:link
 call php artisan migrate
 call php artisan db:seed
 start http://127.0.0.1:8000
-start cmd /k "npm install"
 start cmd /k "php artisan serve"
-start cmd /k "npm run dev"
